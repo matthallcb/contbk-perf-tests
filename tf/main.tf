fc93e7800e982050ad5112602d1a45e577be2481
Matt Hall <matt.hall@couchbase.com>
2026-05-12 17:04:47 +0100

    Use old ubuntu image to avoid TCP buffer regression

--- tf/main.tf
+++ tf/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20251212"]
   }
 
   filter {
